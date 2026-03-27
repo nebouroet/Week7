a22437cd2dc08b20e8a736e86fe9086da19c24f6
Kira <nebouroet@gmail.com>
2026-03-27 08:19:59 +0100

    Updated login test and added fixtures

---
+++ tests/Week7/fixtures/test-fixtures.ts
@@ -0,0 +1,26 @@
+import { test as base } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+import { HomePage } from '../pages/HomePage';
+
+type MyFixtures = {
+  loginPage: LoginPage;
+  homePage: HomePage;
+};
+
+export const test = base.extend<MyFixtures>({
+
+      loginPage: async ({ page }, use) => {
+        const loginPage = new LoginPage(page);
+            await loginPage.goto();
+
+        await use(loginPage);
+
+    },  
+      homePage: async ({ page }, use) => {
+    const homePage = new HomePage(page);
+    await homePage.navigate();
+    await use(homePage);
+  },
+});
+
+export { expect } from '@playwright/test';
--- tests/Week7/tests/login2.spec.ts
+++ tests/Week7/tests/login2.spec.ts
@@ -1,35 +1,29 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '../fixtures/test-fixtures';
 import { LoginPage } from '../pages/LoginPage';
 
-test.describe('Login Tests', () => {
-    let loginPage: LoginPage;
 
-    test.beforeEach(async ({ page }) => {
-        loginPage = new LoginPage(page);
-        await loginPage.goto();
-    });
 
-    test('valid login succeeds', async ({ page }) => {
+    test('valid login succeeds', async ({ loginPage, page } ) => {
         await loginPage.login('aguspe', '12341234');
         await expect(page).toHaveURL('/index.php?rt=account/account');
     });
 
-    test('invalid login', async ({ page }) => {
+    test('invalid login', async ({ loginPage, page } ) => {
         await loginPage.login('invaliduser', 'invalidpassword');
         await expect (page.getByText('Error: Incorrect login or')).toBeVisible()
     });
 
-    test('empty login fields', async ({ page }) => {
+    test('empty login fields', async ({ loginPage, page }) => {
         await loginPage.login('', '12341234');
         await expect(loginPage.usernameInput).toHaveAttribute('required', '');
         await expect(loginPage.passwordInput).toHaveAttribute('required', ''); 
        });
 
-    test('empty password field', async ({ page }) => {
+    test('empty password field', async ({ loginPage, page }) => {
         await loginPage.login('aguspe', '');
         await expect(loginPage.usernameInput).toHaveAttribute('required', '');
         await expect(loginPage.passwordInput).toHaveAttribute('required', ''); 
     })
-})
+
 
 
